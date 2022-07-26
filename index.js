f4504bffbbc98095091a11a24efa807b14c0cada
shoaibtechs <p200147@pwr.nu.edu.pk>
2022-07-26 06:16:37 +0500

    Adding index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+// Now we want to import the "second.js"  
+// file and these type of imports are called as "common js modules"
+const importFile = require("./second");  
+
+
+ console.log("Hello World");
+
+ console.log(importFile);
+
+
+ 
+  
