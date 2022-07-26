b871266745f3314e5060791bb169927c415efe83
shoaibtechs <p200147@pwr.nu.edu.pk>
2022-07-26 06:23:09 +0500

    Adding path module file

---
+++ pathmodule.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const a1 = path.basename('C:\\temp\\myfile.html');
+console.log(a1);
+
+
+const a2 = path.dirname('C:\\temp\\myfile.html');
+console.log(a2);
+
+const a3 = path.extname(__filename)
+
+console.log(a3)
+
