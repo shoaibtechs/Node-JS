065318dc97ccecf14308018c8ad48917a2f7029d
shoaibtechs <p200147@pwr.nu.edu.pk>
2022-07-26 06:16:03 +0500

    Adding httpserver.js file

---
+++ httpserver.js
@@ -0,0 +1,24 @@
+//Creating a HTTP srever 
+
+
+// const http = require('http');
+
+// const port = process.env.PORT || 3000;
+
+// const server = http.createServer((req,res)=>{
+
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type',"text/html")
+//     res.end("<h1>Welcome to shoaibtechs.com </h1> <p> Shoaibtechs is one of the greatest website </p> ");
+
+
+
+
+// });
+
+// server.listen(port,()=>{
+
+//     console.log(`Server is listening on port ${port}`);
+
+
+// });
