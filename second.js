a30683604852dac421a2336a62a642208c79060d
shoaibtechs <p200147@pwr.nu.edu.pk>
2022-07-26 06:23:50 +0500

    secondjs file

---
+++ second.js
@@ -0,0 +1,17 @@
+// Module rapper function
+
+
+
+shoaib = 
+{
+    name: "shoaib",
+    favNumN: 3,
+    favGame:"Cricket"
+
+}
+
+//And also if we want that this file should be imported in another file
+// So, we also have to export this file.. 
+
+module.exports = shoaib;
+
