90a748e9d5ddbd700f279dec1684facd89f9ab1c
shoaibtechs <p200147@pwr.nu.edu.pk>
2022-07-26 06:19:35 +0500

    Addinf ModuleSecond.mjs file

---
+++ moduleSecond.mjs
@@ -0,0 +1,35 @@
+//Common js method to export the file...
+
+// function simple()
+// {
+//     console.log("Hello From moduleSecond.js");
+
+
+// }
+
+//  module.exports = simple;
+
+
+//ES6 Module to export file
+
+
+export function simple()
+{
+    console.log("Hello From moduleSecond.js");
+    return 45;
+
+}
+
+
+
+export default function simple2()
+{
+    console.log("Hello From moduleSecond.js and  simple2");
+
+
+}
+
+
+
+
+
