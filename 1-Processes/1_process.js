d2b3a8333cdb7dab1a19e5e158b38a6f4d155b89
shoaibtechs <p200147@pwr.nu.edu.pk>
2025-10-16 18:22:01 +0500

    Add Process Files

---
+++ 1-Processes/1_process.js
@@ -0,0 +1,13 @@
+//console.log(process);
+
+
+
+// The user will pass the arguments at the time pf compilation and now i am storing those arguments in an array.
+let arr =  process.argv;
+
+
+for(let i=0; i<arr.length; i++){
+
+    console.log("Hello, ", arr[i]);
+
+}
